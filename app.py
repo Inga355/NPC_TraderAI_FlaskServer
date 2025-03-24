41f66d88a0258e803606f4e317965bdd46a9568e
Inga355 <albi1303@gmail.com>
2025-03-24 14:05:43 +0100

    added dependencies, loading APIkey, connecting to database

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, jsonify
+from dotenv import load_dotenv
+import sqlite3
+import openai
+import os
+
+app = Flask(__name__)
+
+# OpenAI API Key
+load_dotenv()
+API_KEY = os.getenv("OPENAI_API_KEY")
+openai.api_key = API_KEY
+
+# Database connection
+DB_NAME = "database.db"
+
+
+def get_db_connection():
+    conn = sqlite3.connect(DB_NAME)
+    conn.row_factory = sqlite3.Row
+    return conn
+
+
+
